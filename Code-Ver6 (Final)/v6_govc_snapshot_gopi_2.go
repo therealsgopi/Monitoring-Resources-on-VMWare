11f33e62c9225571d4eef579394e6c8c19bc4a0f
S Gopi <gopi.s2020a@vitstudent.ac.in>
2023-05-26 03:52:26 +0530

    Documented the v6 code

--- Code-Ver6 (Final)/v6_govc_snapshot_gopi_2.go
+++ Code-Ver6 (Final)/v6_govc_snapshot_gopi_2.go
@@ -9,6 +9,7 @@ import (
 	"time"
 )
 
+// structure to store individual snapshot details
 type Snapshot struct {
 	name string
 	id   string
@@ -16,12 +17,14 @@ type Snapshot struct {
 	date time.Time
 }
 
+// declaring and instantiating global variables
 var (
 	vm string
 	action string
 	snaps = []Snapshot{}
 )
 
+// function to convert size of snapshots to MB from KB & GB
 func sizeToMB(size string) float64 {
 	sizeMB, _ := strconv.ParseFloat(size[0:len(size)-2], 8)
 	if string(size[len(size)-2]) == "K" {
@@ -32,6 +35,7 @@ func sizeToMB(size string) float64 {
 	return sizeMB
 }
 
+// function to return current date in the same format as creation dates of the snapshots
 func currentDate() time.Time {
 	date := time.Now()
 	loc, _ := time.LoadLocation("UTC")
@@ -40,6 +44,7 @@ func currentDate() time.Time {
 	return date
 }
 
+// function to compute the age of a snapshot from current date
 func snapLife(creationDate time.Time) int64 {
 	snap_lifespan := currentDate().Sub(creationDate)
 	days := int64(snap_lifespan.Hours() / 24)
@@ -49,8 +54,9 @@ func snapLife(creationDate time.Time) int64 {
 	return days
 }
 
+// function to get the snapshot details of a specific VM
 func getVMSnapDetails() {
-	//resetting structure "snaps"
+	//resetting structure SNAPS
 	snaps = snaps[0:0]
 
 	// Specify the "govc" command and its arguments
@@ -65,19 +71,24 @@ func getVMSnapDetails() {
 	output_size, _ := cmd_size.Output()
 	output_crDate, _ := cmd_crDate.Output()
 
+	// splitting the output to form an array of individual lines of details for each snapshot
 	lines_ID := strings.Split(strings.TrimSuffix(string(output_ID), "\n"), "\n")
 	lines_name := strings.Split(strings.TrimSuffix(string(output_name), "\n"), "\n")
 	lines_size := strings.Split(strings.TrimSuffix(string(output_size), "\n"), "\n")
 	lines_crDate := strings.Split(strings.TrimSuffix(string(output_crDate), "\n"), "\n")
 
+	// initializing empty SNAPS structure to avoid IndexOutOfBound error
 	snaps = make([]Snapshot, len(lines_ID))
 
+	// storing all the read details of snapshots
 	storeSnapDetails(lines_ID, "ID")
 	storeSnapDetails(lines_name, "name")
 	storeSnapDetails(lines_size, "size")
 	storeSnapDetails(lines_crDate, "crDate")
 }
 
+// function to store all the details of 
+//all the snapshots in the structure SNAPS
 func storeSnapDetails(lines []string, detail string) {
 	for i := 0; i < len(lines); i++ {
 		if detail == "name" {
@@ -101,6 +112,8 @@ func storeSnapDetails(lines []string, detail string) {
 	}
 }
 
+// function to display all the details 
+// of all the snapshots in the structure SNAPS
 func dispSnapDetails() {
 	fmt.Println("Number of snapshots: ", len(snaps))
 	for snap := range snaps {
@@ -113,10 +126,14 @@ func dispSnapDetails() {
 	}
 }
 
+// function to delete the snapshot details from the 
+// structure SNAPS when it is deleted from the storage
 func deleteSnapFromStruct(index int) {
 	snaps = append(snaps[:index], snaps[index+1:]...)
 }
 
+// function to check all the snapshots stored in 
+// the structure SNAPS and take necessary action
 func checkSnapshots(action string) {
 	for snap := range snaps {
 		var snap_rem_flag int64
@@ -135,6 +152,7 @@ func checkSnapshots(action string) {
 			}
 		}
 
+		// taking action if the snapshot is marked illegal
 		if snap_rem_flag == 1 {
 			if action == "delete" {
 				cmd_snap_rem := exec.Command("govc", "snapshot.remove", "-vm", vm, snaps[snap].name)
@@ -148,12 +166,14 @@ func checkSnapshots(action string) {
 	}
 }
 
+// initializing the values entered for the variables through CLI
 func init() {
 	flag.StringVar(&vm, "vm", "Host2_Mint3", "name of the vm whose snapshots are to be checked")
 	flag.StringVar(&action, "action", "warn", "specify if snapshots are to be deleted or warned")
 	flag.Parse()
 }
 
+// Driver Code
 func main() {
 	getVMSnapDetails()
 	
